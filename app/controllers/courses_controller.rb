dae6edef42090895099c259ab43ddd4ee1caa505
emilyluzhang <55498035+emilyluzhang@users.noreply.github.com>
2024-05-11 22:57:37 +0000

    done

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -10,45 +10,50 @@ class CoursesController < ApplicationController
 
     render({ :template => "courses/show" })
   end
-=begin
 
   def create
     @course = Course.new
     @course.title = params.fetch("query_title")
-    @course.term_offered = params.fetch("query_term_")
+    @course.term_offered = params.fetch("query_term")
     @course.department_id = params.fetch("query_department_id")
 
+    @course.save 
+    redirect_to("/courses")
+  end 
+=begin
     if @course.valid?
       @course.save
       redirect_to("/courses", { :notice => "Course created successfully." })
     else
       redirect_to("/courses", { :notice => "Course failed to create successfully." })
     end
-  end
-
+=end
   def update
     id = params.fetch("path_id")
-    @course = Course.where({ :id => the_id }).at(0)
+    @course = Course.where({ :id => id }).at(0)
 
     @course.title = params.fetch("query_title")
     @course.term_offered = params.fetch("query_term_offered")
     @course.department_id = params.fetch("query_department_id")
 
+    @course.save
+
+    redirect_to("/courses/#{id}")
+=begin
     if @course.valid?
       @course.save
       redirect_to("/courses/#{@course.id}", { :notice => "Course updated successfully."} )
     else
       redirect_to("/courses/#{@course.id}", { :alert => "Course failed to update successfully." })
     end
+=end 
   end
-
   def destroy
-    the_id = params.fetch("path")
+    the_id = params.fetch("path_id")
     @course = Course.where({ :id => the_id }).at(0)
 
     @course.destroy
 
     redirect_to("/courses", { :notice => "Course deleted successfully."} )
   end
-=end 
 end
